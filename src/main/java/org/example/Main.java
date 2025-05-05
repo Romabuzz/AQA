3ff0ec1859dc4d276a5a022883f01e2e9f806e75
Roman Buzunov <romabuzz@yandex.ru>
2025-05-05 10:58:31 +0300

    Задание "Циклы"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,38 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+class Main {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) {
+        task1();
+        task2();
+    }
+
+    // Массив до 5
+    public static void task1() {
+        int[] numbers = new int[7];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i + 1;
+        }
+
+        System.out.println();
+        for (int num : numbers) {
+            System.out.println(num);
+            if (num == 5) {
+                break;
+            }
+        }
+    }
+
+    // 10 значений
+    public static void task2() {
+        String[] stringArray = {
+                "Колбаса", "Сыр", "Сосиски", "Сухарики", "Помидоры",
+                "Огурцы", "Капуста", "Сельдерей", "Редис", "Картошка"
+        };
+
+        System.out.println();
+        for (String str : stringArray) {
+            System.out.println(str);
         }
     }
 }
